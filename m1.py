085a5dc3ed3f8fd1335d4b1db445fb61c6e0c6fb
sarikakanuganti <sarika25in@gmail.com>
2022-07-19 14:15:43 +0530

    this is my first code to push

---
+++ m1.py
@@ -0,0 +1,9 @@
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://sarika:deeshu12@cluster0.y7x9p.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+
+d = {"name":"sarika","email":"sarika25in@gmail.com","surname":"kanuganti"}
+db1 = client["mongotest"]
+coll = db1["test"]
+coll.insert_one(d)
